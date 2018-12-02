fb8d7674bbd32a588686783f9059409f8ced2f86
Korophona <klonoa12@gmail.com>
2018-12-02 00:06:11 -0800

    Day 2; Part 2 Completed

--- Day 2/inventory-management.js
+++ Day 2/inventory-management.js
@@ -4,6 +4,7 @@ const processInventory = (event) => {
 
 	reader.onload = () => {
 		const boxes = reader.result.split('\n');
+		boxes.sort();
 		console.log(boxes);
 
 		//Part One
@@ -53,6 +54,35 @@ const makeChecksum = boxes => {
 	return twoCount * threeCount;
 };
 
-const checkCommonLetters = (boxes) => {
+const checkCommonLetters = boxes => {
+	const collectionLength = boxes.length;
+	const copy = boxes.slice();
 
+	for(let box = 0; box < collectionLength-1; box++) {
+		for(let comp = box+1; comp < collectionLength-1; comp++) {
+
+
+			const charsInABox = copy[box].split('');
+			const boxToCompare = copy[comp].split('');
+			let diffs = 0, diffIndex = 0, pos = 0;
+
+
+			while(pos !== boxToCompare.length && diffs < 2) {
+				if(!(charsInABox[pos] === boxToCompare[pos])){
+					diffIndex = pos;
+					diffs++;
+				}
+				pos++;
+			}
+
+
+			if(diffs === 1) {
+				const ID = copy[box].split('');
+				ID.splice(diffIndex, 1);
+				return ID.join('');
+			}
+
+
+		}
+	}
 };
